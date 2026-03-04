579027958db67997f2484fca170def2b7b801fb6
hrsk <hrsk.yegor@gmail.com>
2026-03-04 13:19:58 +0300

    chore: add fonts, icons, global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import { App } from "./app";
+
 import "./index.scss";
-import { App } from "@/app";
 
 createRoot(document.querySelector("#root")!).render(
   <StrictMode>
